48e4a5a24d055dde487562c71ff623ac9b527177
tonmoy227 <145573783+tonmoy227@users.noreply.github.com>
2025-07-26 18:30:24 +0600

    home 3 15%
    home 3 15%

--- assets/js/script.js
+++ assets/js/script.js
@@ -844,8 +844,55 @@ CTAFooter
 })
 
 
+if($('.count-box').length){
+	$('.count-box').appear_c(function(){
+		var $t = $(this),
+		n = $t.find(".count-text").attr("data-stop"),
+		r = parseInt($t.find(".count-text").attr("data-speed"), 10);
+		if (!$t.hasClass("counted")) {
+			$t.addClass("counted");
+			$({
+				countNum: $t.find(".count-text").text()
+			}).animate({
+				countNum: n
+			}, {
+				duration: r,
+				easing: "linear",
+				step: function() {
+					$t.find(".count-text").text(Math.floor(this.countNum));
+				},
+				complete: function() {
+					$t.find(".count-text").text(this.countNum);
+				}
+			});
+		}
+	},{accY: 0});
+};
 
-
+if($('.dial').length){
+	$('.dial').appear_c(function(){
+		var elm = $(this);
+		var color = elm.attr('data-fgColor');  
+		var perc = elm.attr('value'); 
+		var thickness = elm.attr('thickness');  
+		elm.knob({ 
+			'value': 0, 
+			'min':0,
+			'max':100,
+			'skin':'tron',
+			'readOnly':true,
+			'thickness':.1,
+			'dynamicDraw': true,
+			'displayInput':false
+		});
+		$({value: 0}).animate({ value: perc }, {
+			duration: 3500,
+			easing: 'swing',
+			progress: function () { elm.val(Math.ceil(this.value)).trigger('change');
+		}
+	});
+	},{accY: 0});
+}
 
 
 })(jQuery);
